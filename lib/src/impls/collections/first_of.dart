c8934ca2521a29ac9500b06f2e01a77529b5ac33
@mahmoud.abdulrazek <mahmoud.abdulrazek@troylab.org>
2025-09-05 16:40:59 +0300

    Added docs to first of and last of

--- lib/src/impls/collections/first_of.dart
+++ lib/src/impls/collections/first_of.dart
@@ -1,6 +1,14 @@
 import '../../abstractions/scalar_envelope.dart';
 
+/// A [Scalar] that returns the first element of the [values] iterable, or [defaultValue] if the iterable is empty.
+///
+/// This class wraps an iterable of elements and provides a way to retrieve the first element of the iterable.
+/// If the iterable is empty, [defaultValue] is returned instead.
 final class FirstOf<E> extends ScalarEnvelope<E> {
+  /// Creates a new [FirstOf] scalar that returns the first element of [values], or [defaultValue] if the iterable is empty.
+  ///
+  /// The [values] parameter is the iterable of elements to be wrapped.
+  /// The [defaultValue] parameter is the value to be returned if the iterable is empty.
   FirstOf(Iterable<E> values, {required E defaultValue})
     : super(scalar: () => values.firstOrNull ?? defaultValue);
 }
--- lib/src/impls/collections/last_of.dart
+++ lib/src/impls/collections/last_of.dart
@@ -1,6 +1,14 @@
 import '../../abstractions/scalar_envelope.dart';
 
+/// A [Scalar] that returns the last element of the [elements] iterable, or [defaultValue] if the iterable is empty.
+///
+/// This class wraps an iterable of elements and provides a way to retrieve the last element of the iterable.
+/// If the iterable is empty, [defaultValue] is returned instead.
 final class LastOf<E> extends ScalarEnvelope<E> {
+  /// Creates a new [LastOf] scalar that returns the last element of [elements], or [defaultValue] if the iterable is empty.
+  ///
+  /// The [elements] parameter is the iterable of elements to be wrapped.
+  /// The [defaultValue] parameter is the value to be returned if the iterable is empty.
   LastOf(Iterable<E> elements, {required E defaultValue})
     : super(scalar: () => elements.lastOrNull ?? defaultValue);
 }
